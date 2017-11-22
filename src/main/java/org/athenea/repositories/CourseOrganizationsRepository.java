3ebf4260643e3736db32306d16e72b9a5bd5aefd
Unai Zulaika <unaizuzu@gmail.com>
2017-11-22 18:44:47 +0100

    Updated findAll

--- src/main/java/org/athenea/repositories/CourseOrganizationsRepository.java
+++ src/main/java/org/athenea/repositories/CourseOrganizationsRepository.java
@@ -6,7 +6,6 @@ import org.athenea.model.CourseOrganizations;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CourseOrganizationsRepository extends MongoRepository<CourseOrganizations, String> {
-    List<CourseOrganizations> findAll();
     CourseOrganizations findByTitle(String title);
 
 }
