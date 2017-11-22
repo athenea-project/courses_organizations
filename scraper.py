4c24cf9a14c8ecf2bf182d0142f197343e18acad
root <root@vps478091.ovh.net>
2017-11-22 17:27:27 +0100

    Fixed scraper and compose

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import feedparser, json, pymongo
 
 d = feedparser.parse('http://www.spri.eus/euskadinnova/modulos/RSSGenerar.aspx?language=es&tematica=2,3,4,5&tipo=2')
-connection = pymongo.MongoClient("localhost:27017")
-db = client.athenea
+connection = pymongo.MongoClient("localhost:27018")
+db = connection.athenea
 collection = db.courses_organizations
 #cursos = []
 
@@ -10,7 +10,7 @@ for post in d.entries:
     if post.title[0:7] != 'JORNADA':
         c_new = {'title' : post.title , 'description' : post.description.split('</p>')[0], 'date' : (post.description.split('Fecha: <strong>')[1]).split('</strong>')[0], 'place' : (post.description.split('Lugar: <strong>')[1]).split('</strong>')[0]}
         collection.insert_one(c_new)
-        cursos.append(c_new)
+        #cursos.append(c_new)
     pass
 
 #with open('test.json', mode='w', encoding='utf-8') as feedsjson:
